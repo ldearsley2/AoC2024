2128a2a5ce68242180980bca9ae59d5aa033b69e
ldearsley2 <ldearsley2@gmail.com>
2025-04-02 20:05:47 +0100

    day3 p1

--- day3/main.go
+++ day3/main.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	fmt.Println(partOne("test_input.txt"))
+	fmt.Println(partOne("input.txt"))
 }
 
 func partOne(filename string) int {
@@ -20,31 +21,31 @@ func partOne(filename string) int {
 	}
 	defer file.Close()
 
-	var input string
+	var input []string
 	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		input = line
+		input = append(input, line)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-
-	matches := re.FindAllStringSubmatch(input, -1)
 	total := 0
-	for _, match := range matches {
-		first_num, err := strconv.Atoi(match[1])
-		if err != nil {
-			log.Fatal(err)
+	for _, line := range input {
+		matches := re.FindAllStringSubmatch(line, -1)
+		for _, match := range matches {
+			first_num, err := strconv.Atoi(match[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			second_num, err := strconv.Atoi(match[2])
+			if err != nil {
+				log.Fatal(err)
+			}
+			total += first_num * second_num
 		}
-		second_num, err := strconv.Atoi(match[2])
-		if err != nil {
-			log.Fatal(err)
-		}
-		total += first_num * second_num
 	}
-
 	return total
 }
